cce93403f53c7a423dc21b3113d11df99ad2893b
pikDjokr <pushpendubiswas@gmail.com>
2021-09-11 01:36:27 +0530

    Query selector

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-//const element = document.getElementById('selectme')
-//const element = document.getElementsByClassName('heading')
-//const element = document.getElementsByTagName('h2')
 const element = document.querySelector('#selectme')
-console.log(element)
+// element.innerHTML = 'This is from JS'
+// element.style.color = "cyan"
+// element.style.fontSize = "40px"
+// element.style = 'display:none'
+
+// console.log(element.classList)
+// element.classList.add('five')
+// console.log(element.classList)
+// element.classList.remove('one')
+// console.log(element.classList)
+
+const body = document.querySelector('body')
+console.log(body.classList.toggle('dark'))
+const value = element.classList.contains('three')
+console.log(value)
