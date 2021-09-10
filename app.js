30e8d2bcadb3b8d0f548022a7a44d6b0325573e7
pikDjokr <pushpendubiswas@gmail.com>
2021-09-10 11:43:22 +0530

    functions

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-//Data types
-// Primitive or non primitive
+let Name ="vaishnavi"
 let age = 19
-let Name = 'asdf'
-let brand
-let arr = [1,2,3,4,5]
-arr.push("value")
-arr.unshift(21)
-console.log(arr)
-console.log(typeof arr)
+
+console.log(Name.toUpperCase()) 
