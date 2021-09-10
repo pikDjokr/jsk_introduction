ebe599437dfc94717a011dcf88450b37a2961825
pikDjokr <pushpendubiswas@gmail.com>
2021-09-11 02:18:03 +0530

    button toggle

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const element = document.querySelector('#selectme')
-// element.innerHTML = 'This is from JS'
-// element.style.color = "cyan"
-// element.style.fontSize = "40px"
-// element.style = 'display:none'
-
-// console.log(element.classList)
-// element.classList.add('five')
-// console.log(element.classList)
-// element.classList.remove('one')
-// console.log(element.classList)
-
+const button = document.querySelector('button')
 const body = document.querySelector('body')
-console.log(body.classList.toggle('dark'))
-const value = element.classList.contains('three')
-console.log(value)
+button.addEventListener('click',() => {
+    body.classList.toggle('dark')
+})
