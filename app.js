15914a33b73875a3f848c3312ca42aba02ea4847
pikDjokr <pushpendubiswas@gmail.com>
2021-09-10 12:11:59 +0530

    arithmatic operators

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-//Data types
-// Primitive or non primitive
-let age = 19
-let Name = 'asdf'
-let brand
-let arr = [1,2,3,4,5]
-arr.push("value")
-arr.unshift(21)
-console.log(arr)
-console.log(typeof arr)
+//Arithmatic operators
+const a = 2
+const b = 15
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+console.log(b%a)
+console.log("-------------------------------")
+let c =5
+c **= 3
+console.log(c)
