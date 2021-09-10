368da865447a91bfd916e1a3e9f86ab15161e7fa
pikDjokr <pushpendubiswas@gmail.com>
2021-09-10 12:58:02 +0530

    logical operators

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-//Comparison operators
-
-const a = 21
-const b = 7
-const c = 21
-console.log('a>b : ',a>b)
-console.log('a<b : ' , a<b)
-console.log('a==b : ' , a==b)
-console.log('a==c : ', a==c)
-console.log('a>=c : ', a>=c)
-console.log('-------------------------------------------')
-const e = 11
-const d = '11'
-console.log('e==d : ',e==d)
-console.log('e===d : ',e===d)
-console.log('nistha' == 'Nistha')
+//Logical operators
+const a = 10
+const b = 50
+const c = 35
+console.log(c>a || c<b)
+// console.log('a == b : ', a == b)
+// console.log('a!=b : ',       a!=b)
+// console.log('Truth table for AND: ')
+// console.log('false && false : ', false && false)
+// console.log('true && true : ', true && true)
+// console.log('true && false : ', true && false)
+// console.log('false && true : ', false && true)
+// console.log('Truth table for OR: ')
+// console.log('false || false : ', false || false)
+// console.log('true || true : ', true || truee)
+// console.log('true || false : ', true || false)
+// console.log('false || true : ', false || true)
