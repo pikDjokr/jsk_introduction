dce1f7ae006de410a0326478084fd1753c53fc56
pikDjokr <pushpendubiswas@gmail.com>
2021-09-12 12:04:04 +0530

    for and while loops

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
-const rock =  document.querySelector('#rock')
-const paper =  document.querySelector('#paper')
-const scissor =  document.querySelector('#scissor')
-const board = document.querySelector('#board')
-const handler = (e) => {
-    const action = e.target.id
-    // if(action === 'rock'){
-    //     board.innerHTML = 'Rock crushes scissor.'
-    // }else if(action === 'paper'){
-    //     board.innerHTML = 'Paper traps rock.'
-    // }else if(action === 'scissor'){
-    //     board.innerHTML = 'Scissor cuts paper.'
-    // }else{
-    //     board.innerHTML = 'Click something'
-    // }
 
-    switch(action){
-        case 'rock':
-            board.innerHTML = 'Rock crushes scissor.'
-            break
-        case 'paper':
-            board.innerHTML = 'Paper traps rock.'
-            break
-        case 'scissor':
-            board.innerHTML = 'Scissor cuts paper.'
-            break
-        default:
-            board.innerHTML = 'Click something.'
-    }
+// for(var i=1;i<=10;i++){
+//     console.log('iteration ' +i)
+// }
+// console.log(i)
+
+let iteration = 1
+while(iteration<10){
+    console.log(iteration++)
+    iteration++
 }
-//handler('rock')
-rock.addEventListener('click',handler)
-paper.addEventListener('click',handler)
-scissor.addEventListener('click',handler)
-
-console.log('One')
-
-setTimeout(() => {
-    console.log('Two')
-},5000)
-console.log('Three')
